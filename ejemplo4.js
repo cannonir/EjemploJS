113030749615e72e0b7bef577b6b19e2296373d9
unknown <eldel.xperia@gmail.com>
2016-10-21 08:39:45 -0400

    iniciando repositorio

---
+++ ejemplo4.js
@@ -0,0 +1,7 @@
+console.log("suma de numeros 13+7:  " + (13+7));
+//console.log(13+7);
+console.log("resta de numeros 13-7:");
+console.log(13-7);
+console.log("expresion con parentesis (8*2-4)/3: ");
+console.log((8*2-4)/3);
+
---
+++ ejemplo5.js
@@ -0,0 +1,8 @@
+console.log("\"hola\"");
+console.log('"hola"');
+console.log("suma de numeros 13+7: ");
+console.log(13+7);
+console.log("signo de un numero +13:");
+console.log(+13);
+console.log("conexion de strings\'hola\"+\"Pepe\" ;");
+console.log("hola "+" Pepe");
